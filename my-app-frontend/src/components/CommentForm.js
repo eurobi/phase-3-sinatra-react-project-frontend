3f3ee5547e911d016c4dc33bee7e76ecca2707dc
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-11-07 09:42:23 -0500

    comments working on front end

--- my-app-frontend/src/components/CommentForm.js
+++ my-app-frontend/src/components/CommentForm.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 
-function CommentForm({user, post}){
+function CommentForm({user, post, setPosts, posts}){
     const [comment, setComment] = useState("")
     function handleSubmit(e){
         e.preventDefault()
-        console.log(comment)
+        console.log(posts)
+        fetch('http://localhost:9292/comments',{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                content: comment,
+                user_id: user.id,
+                post_id: post.id
+            })
+        })
+        .then(r => r.json())
+        .then(returnedComment => {
+            const newPosts = [...posts]
+            const mappedPosts = newPosts.map((mappost) => {
+                if(mappost.id === post.id){
+                    mappost.comments.push(returnedComment)
+                    return mappost
+                }else{
+                    return mappost
+                }
+            })
+            setPosts(mappedPosts)
+        })
     }
     return(
         <form onSubmit={handleSubmit}>
